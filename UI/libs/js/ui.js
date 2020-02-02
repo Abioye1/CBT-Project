8cba9dab47414d02cd090defe417d682c7b1695f
abioye20 <ibrokesh4luv@gmail.com>
2020-02-02 13:20:23 +0100

    interface for student examination courses

--- UI/libs/js/ui.js
+++ UI/libs/js/ui.js
@@ -1,5 +1,5 @@
 
     function validate()
     {
-        window.location.replace('http://127.0.0.1:5500/courses.html');
+        window.location.replace('http://127.0.0.1:5500/UI/courses.html');
     }
